cc7b0f9b9fc035973de82b68766aa6a6b239090d
Anji-6353 <anjicool1308@gmail.com>
2025-03-13 19:57:37 +0530

    modify

--- src/main/java/com/spring/irctc/IrctcTicketBookingApplication.java
+++ src/main/java/com/spring/irctc/IrctcTicketBookingApplication.java
@@ -8,6 +8,7 @@ public class IrctcTicketBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IrctcTicketBookingApplication.class, args);
+		cicbw;bco;ubo
 	}
 
 }
